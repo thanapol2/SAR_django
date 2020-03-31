17e64c0449828e56d8a046cf3b4d4f49a0956d0a
boom102155 <33820649+boom102155@users.noreply.github.com>
2020-03-31 11:45:01 +0700

    update 31/03/2563 (2)
    update 31/03/2563 (2)

--- cpc_project/eva/forms.py
+++ cpc_project/eva/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from django.forms import TextInput, HiddenInput
+from django.forms import TextInput, HiddenInput, Select, NumberInput
 
 from .models import Sub_Cate
+
+TARGET_CHOICES=[
+    ('','-- Select Target --'),
+    ('1','1'),
+    ('2','2'),
+    ('3','3'),
+    ('4','4'),
+    ('5','5'),
+]
 
 class SubCateFormInsert(forms.ModelForm):
     class Meta:
@@ -20,6 +29,14 @@ class SubCateFormInsert(forms.ModelForm):
         self.fields['username'].widget = HiddenInput(attrs={
             'class': 'textinput textInput form-control',
             'value': Username})
+        self.fields['weight'].widget = NumberInput(attrs={
+            'autocomplete':'off',
+            'min': 0,
+            'class': 'form-control'})
+        self.fields['target'].widget = Select(choices=TARGET_CHOICES, attrs={
+            'class': 'form-control'})
+
+
 
 class SubCateFormUpdate(forms.ModelForm):
     class Meta:
